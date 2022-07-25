903c3ccdfc82f122d6d135af2741326e18302892
Grace Joh <graceeunaejoh@gmail.com>
2022-07-24 21:15:35 -0400

    valid anagram

---
+++ validanagram.py
@@ -0,0 +1,23 @@
+class Solution:
+  def isAnagram(self, s: str, t: str) -> bool:
+    chars = {}
+    for char in s: 
+        if char in chars:
+          chars[char] += 1 
+        else: 
+          chars[char] = 1 
+    for char in t:
+        if char not in chars:
+          return False
+        if char in chars:
+          chars[char] -= 1
+          if chars[char] == 0:
+              del chars[char]
+    if chars: 
+        return False 
+    return True 
+
+s = ("anagram")
+t = ("nagaram")
+ob1 = Solution()
+print(ob1.isAnagram(s, t))
